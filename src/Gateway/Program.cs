f8d7a503bc49f41a3d49db8fe10e3c971246fdf8
Thomas Nuyken <thomas.nuyken@proton.me>
2022-06-05 13:30:16 +0200

    Add Gateway with service discovery integration

--- src/Gateway/Program.cs
+++ src/Gateway/Program.cs
@@ -1,19 +1,21 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using Ocelot.Provider.Eureka;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+builder.Configuration.AddJsonFile($"ocelot.{builder.Environment}.json", optional: true, reloadOnChange: true);
 
+builder.Services
+    .AddOcelot(builder.Configuration)
+    .AddEureka();
+
+// Add services to the container.
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
+await app.UseOcelot();
 
 app.MapControllers();
 
