df19ec3c662f8897aa64001ad9a106952ba6d4cb
Thomas Nuyken <thomas.nuyken@proton.me>
2022-06-06 12:31:14 +0200

    Replace Swashbuckle with NSwag in Cars

--- src/Cars/Program.cs
+++ src/Cars/Program.cs
@@ -3,17 +3,16 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddOpenApiDocument();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseOpenApi();
+    app.UseReDoc(o => o.Path = "/redoc");
 }
 
 app.UseHttpsRedirection();
